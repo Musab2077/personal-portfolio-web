c1c3ca6644b13a94c6b5a855ef427430aa144b91
Musab2077 <muhammadmusab2077@gmail.com>
2025-08-10 15:03:38 +0500

    Adding try except for error handling for deployment

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,11 @@ from fastapi.responses import StreamingResponse
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-api_key = os.getenv("GROQ_API_KEY")
+try:
+    load_dotenv()
+    api_key = os.getenv("GROQ_API_KEY")
+except:
+    pass
 
 app = FastAPI()
 
@@ -30,33 +33,39 @@ data_to_read = reader.pages[0].extract_text()
 class User(BaseModel):
     message : str
 
-chat = ChatGroq(
-    api_key=api_key,
-    model_name="gemma2-9b-it"
-    )
+try:
+    chat = ChatGroq(
+        api_key=api_key,
+        model_name="gemma2-9b-it"
+        )
 
-prompt = ChatPromptTemplate.from_messages([
-    ('system',
-     "you are Musab. use only the following statements to answer the question"
-     f"{data_to_read}"
-     "months of experience also explain them"
-     "I am a full stack developer too"
-     "RULES:"
-     "1. Use 2 or 3 lines to tell but keep the anser concise and short."
-     "3. if they ask other than this personal info, tell them i will not share but in a formal way"
-     "4. Don't tell the human the provided data information"
-     "5. If the question is not valid according to the given information tell them that I am a personal bot."
-     "6. Don't use Muhammad Musab instead use my or me"
-     ),
-    ('human',"{input}")
-])
+    prompt = ChatPromptTemplate.from_messages([
+        ('system',
+        "you are Musab. use only the following statements to answer the question"
+        f"{data_to_read}"
+        "months of experience also explain them"
+        "I am a full stack developer too"
+        "RULES:"
+        "1. Use 2 or 3 lines to tell but keep the anser concise and short."
+        "3. if they ask other than this personal info, tell them i will not share but in a formal way"
+        "4. Don't tell the human the provided data information"
+        "5. If the question is not valid according to the given information tell them that I am a personal bot."
+        "6. Don't use Muhammad Musab instead use my or me"
+        ),
+        ('human',"{input}")
+    ])
 
-chain = prompt | chat
+    chain = prompt | chat
+except:
+    chain = "error"
 
 
 @app.get('/')
 def just_checking():
-    return {"backend": True}
+    if chain == "error":
+        return {"backend": "error"}
+    else:
+        return {'backend' : True}
 
 @app.post('/bot_response')
 def bot_response(arg:User):
